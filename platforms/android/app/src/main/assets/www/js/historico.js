58a124805fc3f80dca654803d6c9c8bf04dde768
Lidianenevez <lidianeneves79@gmail.com>
2019-06-17 02:49:38 -0300

    ajuste e novo plugins

--- platforms/android/app/src/main/assets/www/js/historico.js
+++ platforms/android/app/src/main/assets/www/js/historico.js
@@ -6,15 +6,20 @@ $(document).ready(function() {
 	        'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
 	    },
         success: function(response) {
-        	$(response.treinos).each(function(index, treino) {
-        		$('#historico').append(
-        			`<li style="list-style-type: none;">
-        				<i class="fa fa-arrow-down" aria-hidden="true"></i>${treino.treino.treino}  
-        				<i class="fa fa-hand-o-right playicon" aria-hidden="true"></i> 
-        				<span class="Nomeperfil">${treino.data}</span>
-        			</li>`
-        		)
-        	})
+            if(response.treinos != null) {
+
+            	$(response.treinos).each(function(index, treino) {
+            		$('#historico').append(
+            			`<li style="list-style-type: none;">
+            				<i class="fa fa-arrow-down" aria-hidden="true"></i>${treino.treino.treino}  
+            				<i class="fa fa-hand-o-right playicon" aria-hidden="true"></i> 
+            				<span class="Nomeperfil">${treino.data}</span>
+            			</li>`
+            		);
+            	});
+            } else {
+                $('#historico').append(`<div>Ainda não foram realizados exercícios!</div>`);
+            }
         },
         error: function(erro) {
             console.log('esse caralho não tá funcionadno');
